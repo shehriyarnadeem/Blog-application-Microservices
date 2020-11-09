40bc92033a435d70e7e06f747ff921a87fa1a2f1
shehriyarnadeem <shehriyarnadeemy@gmail.com>
2020-11-10 00:20:18 +0500

    Push last

--- .history/client/src/CommentCreate_20200321011109.js
+++ .history/client/src/CommentCreate_20200321011109.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-export default () => {
-  const [title, setTitle] = useState('');
+export default ({ postId }) => {
+  const [content, setContent] = useState('');
 
   const onSubmit = async event => {
     event.preventDefault();
 
-    await axios.post('http://posts.com/posts/create', {
-      title
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
     });
 
-    setTitle('');
+    setContent('');
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label>Title</label>
+          <label>New Comment</label>
           <input
-            value={title}
-            onChange={e => setTitle(e.target.value)}
+            value={content}
+            onChange={e => setContent(e.target.value)}
             className="form-control"
           />
         </div>
